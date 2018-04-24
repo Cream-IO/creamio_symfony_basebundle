5393d752dd6acd1e7a6f0a765ebf4f1f50b8cdd9
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-04-24 12:25:42 +0200

    ✨ Add extra informations to logs by request processing

---
+++ src/Service/LoggerRequestProcessor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CreamIO\BaseBundle\Service;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class LoggerRequestProcessor
+{
+    /**
+     * @var RequestStack
+     */
+    protected $request;
+
+    /**
+     * RequestProcessor constructor.
+     *
+     * @param RequestStack $request
+     */
+    public function __construct(RequestStack $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @param array $record
+     *
+     * @return array
+     */
+    public function processRecord(array $record): array
+    {
+        $req = $this->request->getCurrentRequest();
+        $record['extra']['client_ip']       = $req->getClientIp();
+        $record['extra']['client_port']     = $req->getPort();
+        $record['extra']['uri']             = $req->getUri();
+        $record['extra']['query_string']    = $req->getQueryString();
+        $record['extra']['method']          = $req->getMethod();
+        $record['extra']['request']         = $req->request->all();
+
+        return $record;
+    }
+}
