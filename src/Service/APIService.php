aa99e96e452d303295afbfa0d4b95b66e76350f2
Quentin Raimbault <qraimbault@tillersystems.com>
2019-01-13 03:20:58 +0100

    💥 Change serializer

--- src/Service/APIService.php
+++ src/Service/APIService.php
@@ -4,12 +4,15 @@ namespace CreamIO\BaseBundle\Service;
 
 use CreamIO\BaseBundle\Exceptions\APIError;
 use CreamIO\BaseBundle\Exceptions\APIException;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\Expression\ExpressionEvaluator;
+use JMS\Serializer\SerializerBuilder;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -76,18 +79,20 @@ class APIService
      *
      * This method is used for GET method since it required to return results
      *
-     * @param mixed      $results    Datas to send as results, can be array of objects or a single object
-     * @param int        $statusCode Response code to send (Constants defined in Response::class)
-     * @param string     $resultsFor ID of the requested object, or identifier for collection requests
-     * @param Request    $request    Handled HTTP request to get method from
-     * @param Serializer $serializer Serializer to use (optional)
+     * @param mixed               $results    Datas to send as results, can be array of objects or a single object
+     * @param int                 $statusCode Response code to send (Constants defined in Response::class)
+     * @param string              $resultsFor ID of the requested object, or identifier for collection requests
+     * @param Request             $request    Handled HTTP request to get method from
+     * @param SerializerInterface $serializer Serializer to use (optional)
      *
      * @return JsonResponse
      */
-    public function successWithResults($results, int $statusCode, string $resultsFor, Request $request, Serializer $serializer = null): JsonResponse
+    public function successWithResults($results, int $statusCode, string $resultsFor, Request $request, SerializerInterface $serializer = null): JsonResponse
     {
         if (null === $serializer) {
-            $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
+            $serializer = SerializerBuilder::create()
+                ->setExpressionEvaluator(new ExpressionEvaluator(new ExpressionLanguage()))
+                ->build();
         }
         $return = [
             'status' => 'success',
@@ -106,17 +111,19 @@ class APIService
      *
      * This method is used for DELETE, POST, PATCH and PUT methods since they do not need any result to return
      *
-     * @param string     $id         Ressource ID
-     * @param int        $statusCode Response code to send (Constants defined in Response::class)
-     * @param Request    $request    Handled HTTP request to get method from
-     * @param Serializer $serializer Serializer to use (optional)
+     * @param string              $id         Ressource ID
+     * @param int                 $statusCode Response code to send (Constants defined in Response::class)
+     * @param Request             $request    Handled HTTP request to get method from
+     * @param SerializerInterface $serializer Serializer to use (optional)
      *
      * @return JsonResponse
      */
-    public function successWithoutResults(string $id, int $statusCode, Request $request, Serializer $serializer = null): JsonResponse
+    public function successWithoutResults(string $id, int $statusCode, Request $request, SerializerInterface $serializer = null): JsonResponse
     {
         if (null === $serializer) {
-            $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
+            $serializer = SerializerBuilder::create()
+                ->setExpressionEvaluator(new ExpressionEvaluator(new ExpressionLanguage()))
+                ->build();
         }
         $return = [
             'status' => 'success',
@@ -134,18 +141,20 @@ class APIService
      *
      * This method is used to handle ressource creation requests since those requests generates a Location header redirecting to the created ressource get URL
      *
-     * @param string     $id             Ressource ID
-     * @param Request    $request        Handled HTTP request to get method from
-     * @param int        $statusCode     Response code to send (Constants defined in Response::class)
-     * @param string     $redirectionURL Location header URL
-     * @param Serializer $serializer     Serializer to use (optional)
+     * @param string              $id             Ressource ID
+     * @param Request             $request        Handled HTTP request to get method from
+     * @param int                 $statusCode     Response code to send (Constants defined in Response::class)
+     * @param string              $redirectionURL Location header URL
+     * @param SerializerInterface $serializer     Serializer to use (optional)
      *
      * @return JsonResponse Success response
      */
-    public function successWithoutResultsRedirected(string $id, Request $request, int $statusCode, string $redirectionURL, Serializer $serializer = null): JsonResponse
+    public function successWithoutResultsRedirected(string $id, Request $request, int $statusCode, string $redirectionURL, SerializerInterface $serializer = null): JsonResponse
     {
         if (null === $serializer) {
-            $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
+            $serializer = SerializerBuilder::create()
+                ->setExpressionEvaluator(new ExpressionEvaluator(new ExpressionLanguage()))
+                ->build();
         }
         $return = [
             'status' => 'success',
