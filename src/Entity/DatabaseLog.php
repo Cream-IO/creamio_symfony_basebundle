c60cc28244285fd719c2b0af4d69c3bf654615b5
Quentin Raimbault <qraimbault@tillersystems.com>
2019-01-15 15:24:55 +0100

    🗃 Change table name

--- src/Entity/DatabaseLog.php
+++ src/Entity/DatabaseLog.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="CreamIO\BaseBundle\Repository\DatabaseLogRepository")
- * @ORM\Table(name="creamio_log")
+ * @ORM\Table(name="log")
  * @ORM\HasLifecycleCallbacks
  */
 class DatabaseLog
